2872ad9bbb3b70c288585b572c8d14c2f93693ce
catherinar <catherinalramos@gmail.com>
2017-05-06 18:25:08 -0500

    Selected photo is passed from MainActivity to ColorActivity

--- app/src/main/java/com/example/user/cvtest1/ColorActivity.java
+++ app/src/main/java/com/example/user/cvtest1/ColorActivity.java
@@ -1,7 +1,15 @@
 package com.example.user.cvtest1;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class ColorActivity extends AppCompatActivity {
 
@@ -9,5 +17,25 @@ public class ColorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_color);
+
+
+        Bitmap bitmap = null;
+
+        Bundle extras = getIntent().getExtras();
+        if(!extras.isEmpty()) {
+            Uri imageUri = Uri.parse(extras.getString(MainActivity.PICTURE_INTENT_EXTRA));
+            String filePath = extras.getString("IMAGE");
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }else{
+            InputStream stream = getResources().openRawResource(R.raw.image1);
+            bitmap = BitmapFactory.decodeStream(stream);
+        }
+
+        ImageView imgOrigIV = (ImageView) findViewById(R.id.withoutColor);
+        imgOrigIV.setImageBitmap(bitmap);
     }
 }
